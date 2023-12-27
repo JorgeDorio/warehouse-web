d2d62abcf5766e3f87a57c85708a1ab24213fa59
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2023-12-27 01:49:26 -0400

    feat: add missing fields suppliers

--- src/Pages/Suppliers/index.tsx
+++ src/Pages/Suppliers/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from "react";
 import { Container } from "../../Components/Container";
 import { TextInput } from "../../Components/TextInput";
 import { Supplier } from "../../Models/Supplier";
+import { Button } from "../../Components/Button";
 
 export const Suppliers = () => {
   const [supplierData, setSupplierData] = useState(new Supplier());
@@ -34,6 +35,46 @@ export const Suppliers = () => {
             maskPosition="$1.$2.$3/$4-$5"
             maxLength={18}
           />
+          <TextInput
+            label="Endereço"
+            name="address"
+            value={supplierData.address}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Cidade"
+            name="city"
+            value={supplierData.city}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Estado"
+            name="state"
+            value={supplierData.state}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Pais"
+            name="country"
+            value={supplierData.country}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="Telefone"
+            name="phone"
+            value={supplierData.phone}
+            onChange={handleChange}
+          />
+          <TextInput
+            label="E-mail"
+            name="email"
+            type="email"
+            value={supplierData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mt-2 flex gap-2">
+          <Button label="Salvar" />
         </div>
       </Container>
     </main>
